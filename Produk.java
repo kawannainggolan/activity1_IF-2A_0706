715570aeddae5f7687866c371f8360754eb79179
kawannainggolan <kawannainggolan19@gmail.com>
2026-07-17 20:17:14 +0700

    Membuat class Produk abstract dasar

---
+++ Produk.java
@@ -0,0 +1,27 @@
+public abstract class Produk {
+    protected String nama;
+    protected double harga;
+    protected int stok;
+    
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public double getHarga() {
+        return harga;
+    }
+    
+    public int getStok() {
+        return stok;
+    }
+    
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+}
